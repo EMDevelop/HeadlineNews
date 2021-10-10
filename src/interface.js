e2f846a851406139ec1f99df38160e922e21716e
EMDevelop <edwardmartin.inbox@gmail.com>
2021-10-10 12:13:55 +0100

    fetch is instantiating a new story per index in the response, aand pushing each to the headlines array

--- src/interface.js
+++ src/interface.js
@@ -3,13 +3,26 @@ document.addEventListener('DOMContentLoaded', () => {
   const headlines = new Headlines();
 
   // Fetch Stories from Guardian via Pipedream
+  // For each story in the array of 10 stories, create a new story object
+  // Add the story object into the headlines object to be displayed on screen
   const HTTPGETStories = () => {
     fetch('https://en2lkcel4jj8w1p.m.pipedream.net')
       .then((response) => response.json())
       .then((data) => {
         data.storys.forEach((story) => {
-          headlines.addStory(story);
+          headlines.addStory(
+            new Story({
+              image: story.fields.thumbnail,
+              title: story.webTitle,
+              body: story.fields.bodyText,
+              originalStory: story.webUrl,
+            })
+          );
         });
+        console.log(headlines.stories);
       });
   };
+
+  // Call Fetch
+  HTTPGETStories();
 });
